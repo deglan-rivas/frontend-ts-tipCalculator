b7100ba9b931f3a000da01637cb202e13c36f192
deglan rivas <deglan.rivas@pucp.edu.pe>
2024-05-30 17:44:49 -0500

    feat: :sparkles: add order provider to main component

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import { OrderProvider } from './context/OrderContext.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <OrderProvider>
+      <App />
+    </OrderProvider>
   </React.StrictMode>,
 )
